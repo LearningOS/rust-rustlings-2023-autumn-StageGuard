985a50991a3127039f7a8b843276dcb13af73b4e
StageGuard <1355416608@qq.com>
2023-10-16 12:59:22 +0800

    fix clippy

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -9,7 +9,6 @@ use std::mem::swap;
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    my_option.unwrap_or_default();
 
     let my_arr = &[
         -1, -2, -3,
@@ -23,7 +22,6 @@ fn main() {
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // Let's swap these two!
-    swap(&mut value_a, &mut value_b);
+    std::mem::swap(&mut value_a,&mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
