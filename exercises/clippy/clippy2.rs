23251e35d42988633f7ee8ba4919eb57895086ef
StageGuard <1355416608@qq.com>
2023-10-16 13:09:28 +0800

    fix

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -3,12 +3,14 @@
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
 // hint.
 
+
+
 fn main() {
     let mut res = 42;
     let option = Some(12);
 
-    if let Some(unwrap) = option {
-        res += unwrap;
+    if let Some(num) = option {
+        res += num;
     }
 
     println!("{}", res);
